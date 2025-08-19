656b0bf65102b0d4ae3d490f2b33419d96f1c1b6
betaxab <betaxab@gmail.com>
2025-08-19 20:27:26 +0800

    update kickstart.sh

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -51,27 +51,46 @@ else
     OUT_ERROR "[错误] 不支持的操作系统！"
 fi
 
+case $(uname -m) in
+    aarch64 | arm64) BARCH=arm64 && ARCH=arm64 ;;
+    x86_64 | amd64) BARCH=amd64 && ARCH=x86_64 ;;
+    *) OUT_ERROR "[错误] 不支持的操作系统！" ;;
+esac
+
 cd ~
 
 OUT_ALERT "[提示] 生成密钥中"
 SECRET=$(openssl rand -hex 12)
 
 OUT_ALERT "[提示] 下载程序中"
-rm -fr release
-wget -O release.zip https://github.com/betaxab/stream/releases/latest/download/release.zip || OUT_ERROR "[错误] 下载程序失败！"
+TMP_DIR=$(mktemp -d)
+cd $TMP_DIR
+STREAM_VER="$(curl -s "https://api.github.com/repos/betaxab/stream/releases" | grep 'tag_name' | head -n 1 | cut -d '"' -f 4)"
+wget -O release.zip "https://github.com/betaxab/stream/releases/download/${STREAM_VER}/stream-linux-${BARCH}.zip" || OUT_ERROR "[错误] 下载程序失败！"
 
 OUT_ALERT "[提示] 解压程序中"
-unzip release.zip && rm -f release.zip && cd release
 
-OUT_ALERT "[提示] 设置权限中"
-chmod +x stream
+unzip release.zip
+
+OUT_ALERT "[提示] 复制程序中"
+install stream /usr/bin
 
 OUT_ALERT "[提示] 复制配置中"
 cp -f default.json /etc/stream.json
 sed -i "s/__API_SECRET__/$SECRET/" /etc/stream.json
 
-OUT_ALERT "[提示] 复制程序中"
-cp -f stream /usr/bin
+OUT_ALERT "[提示] 配置 Logrotate 中"
+cat > /etc/logrotate.d/stream << EOF
+/var/log/stream.log {
+    daily
+    rotate 3
+    compress
+    delaycompress
+    missingok
+    notifempty
+    copytruncate
+}
+EOF
 
 OUT_ALERT "[提示] 配置服务中"
 cat > /etc/systemd/system/stream.service << EOF
@@ -101,5 +120,6 @@ OUT_INFO  "[信息] 部署完毕！"
 OUT_ALERT "[提示] 您的 DNS 地址 $CURRENT:53"
 OUT_ALERT "[提示] 您的 API 密钥 $SECRET"
 OUT_ALERT "[提示] 您的 API 地址 http://$CURRENT:8888/aio?secret=$SECRET"
-cd ~ && rm -fr release
+cd ~ && rm -rf $TMP_DIR
+
 exit 0
